4ef6dce8013b6670fdfd9d10f6691167fd03086a
Marco Ceppi <marco@ceppi.net>
2015-01-23 12:06:09 -0500

    Better file writing, configure logic

--- lib/hooks.py
+++ lib/hooks.py
@@ -44,18 +44,19 @@ def install():
     extract_tar('payload/collector-worker.tar.gz', '/opt/collector-worker')
     subprocess.check_call(['make', '.venv'], cwd='/opt/collector-worker')
 
-    with open('/opt/collector-web/conf/apache/app.conf', 'r+') as f:
+    with open('/opt/collector-web/conf/apache/app.conf', 'r') as f:
         conf = f.read()
 
         conf = "Listen 9000" + '\n' + conf
-        conf = conf.replace('<VirtualHost *:80>', '<VirtualHost *:9000>', conf)
+        conf = conf.replace('<VirtualHost *:80>', '<VirtualHost *:9000>')
         conf = conf.replace('/path/to/app/venv/dir',
-                            '/opt/collector-web/.venv', conf)
+                            '/opt/collector-web/.venv')
         conf = conf.replace('/path/to/dir/containing/wsgi/file',
-                            '/opt/collector-web/conf/apache', conf)
+                            '/opt/collector-web/conf/apache')
 
         with open('/etc/apache2/sites-available/cabs-collector-web.conf',
                   'w') as a:
+            a.truncate()
             a.write(conf)
 
     with open('/opt/collector-web/conf/apache/app.wsgi', 'r+') as f:
@@ -63,12 +64,13 @@ def install():
 
         conf = conf.replace("ini_path = '/path/to/myapp/production.ini'",
                             "ini_path = '/opt/collector-web/production.ini'")
-        conf = conf.replace('<VirtualHost *:80>', '<VirtualHost *:9000>', conf)
+        conf = conf.replace('<VirtualHost *:80>', '<VirtualHost *:9000>')
         conf = conf.replace('/path/to/app/venv/dir',
-                            '/opt/collector-web/.venv', conf)
+                            '/opt/collector-web/.venv')
         conf = conf.replace('/path/to/dir/containing/wsgi/file',
-                            '/opt/collector-web/conf/apache', conf)
+                            '/opt/collector-web/conf/apache')
         f.seek(0, 0)
+        f.truncate()
         f.write(conf)
 
     host.chownr('/opt/collector-web', 'ubuntu', 'ubuntu')
@@ -89,12 +91,17 @@ def configure(force=False):
         contents = re.sub(r'#TIME_ZONE = .*', "TIME_ZONE = 'Etc/UTC'",
                           contents)
         f.seek(0, 0)
-        f.write(new_contents)
+        f.truncate()
+        f.write(contents)
+
+    if 'juju-secret' not in config:
+        return
+
+    if config.changed('juju-user') or config.changed('juju-secret') or config.changed('local-env') or force:
         juju_api = 'wss://localhost:17070'
         if config['local-env']:
             juju_api = 'wss://10.0.3.1:17070'
 
-    if config.changed('juju-user') or config.changed('juju-secret'):
         with open('/opt/collector-web/production.ini', 'r+') as f:
             ini = f.read()
             ini = re.sub(r'juju.api.user = .*',
@@ -104,7 +111,10 @@ def configure(force=False):
             ini = re.sub(r'juju.api.endpoint = .*',
                          'juju.api.endpoint = %s' % juju_api, ini)
             f.seek(0, 0)
+            f.truncate()
             f.write(ini)
+
+        host.service_restart('apache2')
 
 
 def emitter_rel():
