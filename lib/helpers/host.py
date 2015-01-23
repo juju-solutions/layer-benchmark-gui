7574fe1ecaf66f84f0a879f3e15972a60a8295ec
Marco Ceppi <marco@ceppi.net>
2015-01-23 08:26:03 -0500

    Extract tarbal

--- lib/helpers/host.py
+++ lib/helpers/host.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 
 
 def touch(fname):
@@ -6,3 +7,11 @@ def touch(fname):
         os.utime(fname, None)
     else:
         open(fname, 'a').close()
+
+
+def extract_tar(tarbal, dest):
+    if not tarfile.is_tarfile(tarbal):
+        raise ValueError('%s is not a tarbal')
+
+    arch = tarfile.open(tarbal)
+    arch.extractall(dest)
