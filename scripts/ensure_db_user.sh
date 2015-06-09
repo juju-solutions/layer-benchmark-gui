75da35041f750a83bd7fedf77effee7a16694aa3
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-06-09 17:14:42 -0400

    Remove unnecessary escaping

--- scripts/ensure_db_user.sh
+++ scripts/ensure_db_user.sh
@@ -5,7 +5,7 @@
 PSQL='sudo -u postgres psql'
 
 if ! $PSQL -c '\dg' | grep cabs >/dev/null 2>&1 ; then
-  $PSQL -c "CREATE USER cabs WITH UNENCRYPTED PASSWORD \'cabs\';"
+  $PSQL -c "CREATE USER cabs WITH UNENCRYPTED PASSWORD 'cabs';"
 else
   juju-log "Database user 'cabs' already exists, skipping creation."
 fi
