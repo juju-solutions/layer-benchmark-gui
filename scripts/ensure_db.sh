9f75fdc4b5445a0b133cd1bd98c710aa52e0f31d
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-07-01 12:42:47 -0400

    v1.1.0 - Publish benchmarks to external http endpoint
    If publish.url is not set in config, publishing is
    disabled.
    
    Also adds the ability to do db schema upgrades when
    the charm is upgraded, if necessary.

--- scripts/ensure_db.sh
+++ scripts/ensure_db.sh
@@ -4,8 +4,12 @@
 
 PSQL='sudo -u postgres psql'
 
+cd /opt/collector-web
+
 if ! $PSQL -l | grep cabs >/dev/null 2>&1 ; then
   $PSQL -c "CREATE DATABASE cabs WITH OWNER cabs;"
+  .venv/bin/initialize_db production.ini
 else
-  juju-log "Database 'cabs' already exists, skipping creation."
+  juju-log "Database 'cabs' already exists, upgrading..."
+  .venv/bin/alembic -c production.ini upgrade head
 fi
