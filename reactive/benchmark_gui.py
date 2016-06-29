f15f2acc9d04e29e5b583eb55980f6b4414cfb75
George Kraft <cynerva@gmail.com>
2016-06-29 15:30:50 -0500

    fix problem when configuring juju-secret too early

--- reactive/benchmark_gui.py
+++ reactive/benchmark_gui.py
@@ -18,7 +18,6 @@ from charmhelpers import fetch
 from helpers import apache2
 from helpers.host import touch, extract_tar
 
-#TODO: use reactive states instead of hooks
 #TODO: use templates instead of ./files
 
 @hook('install')
@@ -83,11 +82,8 @@ def install_benchmark_gui():
 
 
 @hook('config-changed')
-def configure(force=False):
+def configure():
     config = hookenv.config()
-
-    def changed(key):
-        return force or config.changed(key)
 
     if config.changed('proxy') and config.get('proxy'):
         shutil.rmtree('/opt/collector-web')
@@ -134,20 +130,23 @@ def configure(force=False):
         'graphite.url = http://%s:9001' % hookenv.unit_get('public-address'),
         ini)
 
-    if changed('juju-user'):
-        ini = re.sub(
-            r'juju.api.user =.*',
-            'juju.api.user = %s' % config.get('juju-user') or '', ini)
+    ini = re.sub(
+        r'juju.api.user =.*',
+        'juju.api.user = %s' % config.get('juju-user') or '',
+        ini
+    )
 
-    if changed('juju-secret'):
-        ini = re.sub(
-            r'juju.api.secret =.*',
-            'juju.api.secret = %s' % config.get('juju-secret') or '', ini)
+    ini = re.sub(
+        r'juju.api.secret =.*',
+        'juju.api.secret = %s' % config.get('juju-secret') or '',
+        ini
+    )
 
-    if changed('publish-url'):
-        ini = re.sub(
-            r'publish.url =.*',
-            'publish.url = %s' % config.get('publish-url') or '', ini)
+    ini = re.sub(
+        r'publish.url =.*',
+        'publish.url = %s' % config.get('publish-url') or '',
+        ini
+    )
 
     with open(ini_path, 'w') as f:
         f.write(ini)
@@ -251,5 +250,5 @@ def stop():
 def upgrade():
     shutil.rmtree('/opt/collector-web')
     install_benchmark_gui()
-    configure(True)
+    configure()
     start()
